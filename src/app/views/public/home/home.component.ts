3349515381de28e1d29924fbc683136eeccdf59e
TouyaNguyen <nguyenanh9761@gmail.com>
2025-06-04 11:27:13 +0700

    feat: Popup khi bấm xem chi tiết của sản phẩm refactor: Tái cấu trúc mock data

--- src/app/views/public/home/home.component.ts
+++ src/app/views/public/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Products } from '../../../mock/product.mock';
-import { NgFor, SlicePipe } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { ChunkPipe } from '../../../config/pipe.config';
 import { Foods } from '../../../mock/food.mock';
 import { Drinks } from '../../../mock/drink.mock';
+import { FormsModule } from '@angular/forms';
+import { ProductDetailComponent } from '../../../shared/product-detail/product-detail.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SlicePipe, NgFor, ChunkPipe],
+  imports: [NgFor, ChunkPipe, FormsModule, ProductDetailComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -15,4 +17,13 @@ export class HomeComponent {
   products = Products;
   foods = Foods;
   drinks = Drinks;
+  check = false;
+  selectedId!: string;
+  onHandleViewDetail(id: string) {
+    this.check = true;
+    this.selectedId = id;
+  }
+  onHandleClosePopup() {
+    this.check = false;
+  }
 }
