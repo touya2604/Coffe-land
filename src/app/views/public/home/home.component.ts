f3c2ef038e40af2ca9987fe5e9e521471290200b
TouyaNguyen <nguyenanh9761@gmail.com>
2025-06-06 09:52:22 +0700

    refactor: Sử dụng @for thay vì ngFor để render dữ liệu

--- src/app/views/public/home/home.component.ts
+++ src/app/views/public/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Products } from '../../../mock/product.mock';
-import { NgFor } from '@angular/common';
+import { CurrencyPipe, NgFor } from '@angular/common';
 import { ChunkPipe } from '../../../config/pipe.config';
 import { Foods } from '../../../mock/food.mock';
 import { Drinks } from '../../../mock/drink.mock';
 import { FormsModule } from '@angular/forms';
 import { ProductDetailComponent } from '../../../shared/product-detail/product-detail.component';
 import { CartComponent } from '../cart/cart.component';
+import { chunkArray } from '../../../core/utils/carousel-groups.util';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +17,7 @@ import { CartComponent } from '../cart/cart.component';
     FormsModule,
     ProductDetailComponent,
     CartComponent,
+    CurrencyPipe,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
@@ -32,5 +34,17 @@ export class HomeComponent {
   }
   onHandleClosePopup() {
     this.check = false;
+  }
+
+  groupedProducts: any[][] = [];
+  groupedFood: any[][] = [];
+  groupedDrink: any[][] = [];
+  ngOnInit() {
+    const popularProduct = this.products.filter(
+      (product) => product.isPopular === true
+    );
+    this.groupedProducts = chunkArray(popularProduct, 4);
+    this.groupedFood = chunkArray(this.foods, 4);
+    this.groupedDrink = chunkArray(this.drinks, 4);
   }
 }
