8371d0bc0183b71d5e854ed851c9edf0966230b0
Touya Nguyen <153391569+touya2604@users.noreply.github.com>
2025-06-09 05:47:38 +0700

    view: About us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './views/public/home/home.component';
 import { CartComponent } from './views/public/cart/cart.component';
 import { LoginComponent } from './views/public/login/login.component';
 import { SignUpComponent } from './views/public/sign-up/sign-up.component';
+import { AboutComponent } from './views/public/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'about-us', component: AboutComponent },
 ];
