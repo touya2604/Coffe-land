9f636dd24d793d2e893df0167c97c697fb511619
Touya Nguyen <153391569+touya2604@users.noreply.github.com>
2025-06-08 13:03:56 +0700

    view: Login & Sign up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/public/home/home.component';
 import { CartComponent } from './views/public/cart/cart.component';
+import { LoginComponent } from './views/public/login/login.component';
+import { SignUpComponent } from './views/public/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
 ];
---
+++ src/app/views/public/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  loginType: string = 'email';
+}
