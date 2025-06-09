f804ff5afef6446303b808c8f3ddafe80400d579
Touya Nguyen <153391569+touya2604@users.noreply.github.com>
2025-06-10 05:32:46 +0700

    feat: Login sử dụng mockdata

--- src/app/views/public/login/login.component.ts
+++ src/app/views/public/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Users } from '../../../mock/user.mock';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
+  users = Users;
   loginType: string = 'email';
+  email = '';
+  phone = '';
+  pass = '';
+  onLogin() {
+    for (let i = 0; i < this.users.length; i++) {
+      if (
+        (this.email === this.users[i].email ||
+          this.phone === this.users[i].phone) &&
+        this.pass === this.users[i].pass
+      ) {
+        this.router.navigate(['/home']);
+        break;
+      }
+    }
+  }
 }
