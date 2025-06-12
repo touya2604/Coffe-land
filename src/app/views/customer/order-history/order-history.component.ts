52546722b76eb8caae53df2d150d32fa9a564a81
TouyaNguyen <nguyenanh9761@gmail.com>
2025-06-12 11:54:39 +0700

    refactor: Chỉnh sửa onHandlePay ở view Payment(-normal)

--- src/app/views/customer/order-history/order-history.component.ts
+++ src/app/views/customer/order-history/order-history.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Product } from '../../../model/product.model';
 
 @Component({
   selector: 'app-order-history',
@@ -7,4 +8,13 @@ import { RouterLink } from '@angular/router';
   templateUrl: './order-history.component.html',
   styleUrl: './order-history.component.scss',
 })
-export class OrderHistoryComponent {}
+export class OrderHistoryComponent {
+  orderHistory: (Product & { quantity?: number; checkIn?: Date })[] = [];
+  ngOnInit() {
+    const order = localStorage.getItem('orderHistory');
+    if (order) {
+      this.orderHistory = JSON.parse(order);
+    }
+    console.log(this.orderHistory);
+  }
+}
