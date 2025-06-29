800f84107f4f9c75be8c00159a54211133b21040
Touya Nguyen <nguyenanh9761@gmail.com>
2025-06-29 18:29:54 +0700

    view: Thêm Badges cho Cart Icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { User } from '../../model/user.model';
+import { Product } from '../../model/product.model';
 
 @Component({
   selector: 'app-header',
@@ -11,10 +12,23 @@ import { User } from '../../model/user.model';
 export class HeaderComponent {
   userCurrent!: User;
   constructor(private router: Router) {}
+  checkCart = this.userCurrent ? 'UserCart' : 'cart';
+  cartCurrent: Product[] = [];
   ngOnInit() {
     const user = localStorage.getItem('currentUser');
+    this.checkCart = user ? 'UserCart' : 'cart';
+    this.updateCart(); // Lấy lần đầu
     if (user) {
       this.userCurrent = JSON.parse(user);
+    }
+    setInterval(() => {
+      this.updateCart();
+    }, 1000); // cập nhật mỗi 1 giây
+  }
+  updateCart() {
+    const cart = localStorage.getItem(this.checkCart);
+    if (cart) {
+      this.cartCurrent = JSON.parse(cart);
     }
   }
   onLogOut() {
